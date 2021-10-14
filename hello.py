2c0d72d9dc6a49352649787c709f731976b40d6a
Z. Caner Taskin <caner.taskin@boun.edu.tr>
2021-10-14 09:54:03 +0300

    operators

--- hello.py
+++ hello.py
@@ -9,6 +9,9 @@
 # else:
 #     print("i is neighter")
 
+# in C there is also switch statement
+# match introduced to Python 3.10
+
 print("range(10)")
 for i in range(10):
     if i == 5:
@@ -29,7 +32,57 @@ for i in range(5, 15, 3):
 sum = 0
 w = 15
 while w <= 20:
-    sum = sum + w
-    w = w + 1
+#    sum = sum + w
+    sum += w
+#    w = w + 1
+    w += 1
 
 print (sum)
+
+# operators
+# +, -, *, /, %
+
+i = 12 / 5
+print(i)
+
+j = 12 // 5
+print(j)
+
+k = 2 ** 5
+print (k)
+
+# =, ==, !=, <=, <, >=, >
+
+# logical operators
+# && -> C
+# and 
+# || -> C
+# or 
+# not 
+
+if k > 30 and k < 50:
+    print ("looks good")
+
+# in  
+l = [3, 5, 10, 34] 
+
+if 5 in l:
+    print("found!")
+else:
+    print ("not found")
+
+d = {
+    "IE 201": "Z. Caner Taskin", 
+    "IE 203": "Necati Aras"
+}
+
+check = "IE 201"
+if check in d:
+    print ("I am taking " + check + " from " + d[check])
+else:
+    print ("I am not taking " + check + " this semester")
+
+my_string = "domates"
+
+if "d" in my_string:
+    print ("I found it!")
