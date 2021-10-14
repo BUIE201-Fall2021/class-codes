ab7dbae0fc490d4f2610b93aef28f412de153430
Z. Caner Taskin <caner.taskin@boun.edu.tr>
2021-10-14 09:31:03 +0300

    for and while

--- hello.py
+++ hello.py
@@ -1,13 +1,35 @@
 
-instr = input("Please enter a value")
-i = int(instr)
+# instr = input("Please enter a value")
+# i = int(instr)
 
-if i == 5:
-    print("i is five")
-elif i == 10:
-    print("i is ten")
-else:
-    print("i is neighter")
+# if i == 5:
+#     print("i is five")
+# elif i == 10:
+#     print("i is ten")
+# else:
+#     print("i is neighter")
 
+print("range(10)")
 for i in range(10):
+    if i == 5:
+        continue
+    elif i == 8:
+        break
+    else:
+        print (i)
+
+print("range(5, 15)")
+for i in range(5, 15):
     print (i)
+
+print("range(5, 15, 3)")
+for i in range(5, 15, 3):
+    print (i)
+
+sum = 0
+w = 15
+while w <= 20:
+    sum = sum + w
+    w = w + 1
+
+print (sum)
