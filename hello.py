b576caadc3de6d804207be63b097c9f24fa604ef
Z. Caner Taskin <caner.taskin@boun.edu.tr>
2021-10-14 09:19:08 +0300

    if / elif / else

--- hello.py
+++ hello.py
@@ -1,49 +1,13 @@
-print("Hello, world!")
 
-# This is a single line comment
-# This is another line of the comment
-name = input("Please enter name:")  #everyting after number sign is ignored
-print ("Hello, " + name)
+instr = input("Please enter a value")
+i = int(instr)
 
-"""
-    This is actually a multi-line string
-    But it will be ignored unless we assign it 
-    to a variable. Hence it can be used as a comment.
-"""
+if i == 5:
+    print("i is five")
+elif i == 10:
+    print("i is ten")
+else:
+    print("i is neighter")
 
-# Variable types:
-# bool
-b = True
-f = False
-
-# numerical
-i = 4   #int 
-j = 3.41    #float -> double in C
-# complex numbers
-
-# string
-# either single or double quotes can be used
-s = "Hello"
-s2 = 'world'
-# cannot mix and match s3 = "Hello'
-
-# list
-l = [3, 4, 2, 9]
-print(l)
-
-# list can be heterogeneous 
-l2 = [3, "test", 4.12]
-print(l2)
-l2[1] = 5
-print(l2)
-
-# tuple
-# similar to a list, but cannot be modified
-t = (3, 5)
-# t[1] = "test"
-
-# dictionary
-d = {
-    "IE 201" : "Z. Caner Taşkın",
-    "IE 203" : "Necati Aras"
-}
+for i in range(10):
+    print (i)
