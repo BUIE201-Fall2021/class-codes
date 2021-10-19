4d4be1148915309b33d720291e57fded03c66270
zctaskin <zctaskin@gmail.com>
2021-10-19 13:15:18 +0300

    iterative version of factorial

--- hello.py
+++ hello.py
@@ -1,55 +1,14 @@
-def greet(name = "Stranger"):
-    # statements
-    i = 10  # local variable
-    print("Hello " + name + " I am function")
-    print ("i = " + str(i))
+def factorial(n):
+    result = 1
+    for i in range(1, n+1):
+        result *=  i
+    return result
 
-my_global = "caner"  # global variable -> don't use global variables!
-greet(my_global)
+fact4 = factorial(5)
+print(fact4)
 
-# cannot access local variable outside of the function
-# i += 40 
+fact1 = factorial(1)
+print(fact1)
 
-
-def calculate_area (width, height):
-    return width * height
-
-area = calculate_area(4, 6)
-print(area)
-
-greet() # using default parameters
-
-# optional parameters have to be defined after required parameters
-def my_function(a, b = 0):
-    print (str(a) + " " + str(b))
-
-# positional arguments
-my_function(1, 5)
-my_function(1)
-
-# keyword arguments
-my_function (a = 5, b = 10)
-my_function (b = 10, a = 5)
-
-# arbitrary positional arguments as tuple
-def sum_all(*values):
-    total = 0
-    for value in values:
-        total += value
-    return total
-
-sum = sum_all(4, 5, 5, 6, 2, 6)
-print(sum)
-
-sum = sum_all()
-print(sum)
-
-sum = sum_all(4, 5)
-print(sum)
-
-# arbitrary keyword arguments as dict
-# kwargs
-
-# In C/C++ function overloading is possible
-# multiple functions with the same name but different parameters
-# function overloading is not supported in Python.
+fact0 = factorial(0)
+print(fact0)
