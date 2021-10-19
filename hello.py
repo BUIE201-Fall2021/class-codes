350e0c9d5e18bd7866d6f293637852ff53a43763
zctaskin <zctaskin@gmail.com>
2021-10-19 14:56:24 +0300

    fibonacci_recursive_cached

--- hello.py
+++ hello.py
@@ -18,8 +18,20 @@ def fibonacci_recursive(n):
     # recursion
     return fibonacci_recursive(n-2) + fibonacci_recursive(n-1)
 
+def fibonacci_recursive_cached(n, cache):
+    # base condition
+    if n <= 2:
+        return 1
+    # check cache 
+    if n in cache:
+        return cache[n]
+    # insert into cache 
+    cache[n] = fibonacci_recursive_cached(n-2, cache) + fibonacci_recursive_cached(n-1, cache)
+    return cache[n]
+
 fib3 = fibonacci(3)
 fibrecursive5 = fibonacci_recursive(5)
+fibcached5 = fibonacci_recursive_cached(5, {})
 
 for i in range(1, 40):
     start = time.time()
@@ -31,4 +43,9 @@ for i in range(1, 40):
     fibonacci_recursive(i)
     end = time.time()
     print("Recursive calculation of " + str(i) + " took " + str(end - start))
+
+    start = time.time()
+    fibonacci_recursive_cached(i, {})
+    end = time.time()
+    print("Recursive cached calculation of " + str(i) + " took " + str(end - start))
 
