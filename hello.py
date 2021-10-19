50d6f5b239375cf6b9a911509745b5d064d10c49
zctaskin <zctaskin@gmail.com>
2021-10-19 14:14:52 +0300

    iterative version of fibonacci

--- hello.py
+++ hello.py
@@ -1,43 +1,30 @@
 import time
 
-def factorial(n):
-    result = 1
-    for i in range(1, n+1):
-        result *=  i
-    return result
-
-def factorial_recursive(n):
-    # base condition
-    if n <= 1:
+def fibonacci(n):
+    if n <= 2:
         return 1
-    # recursive step / recursion
-    n1 = factorial_recursive(n-1)
-    return n * n1
+    prev = 1
+    prev2 = 1
+    for i in range(n-2):
+        current = prev + prev2
+        prev2 = prev
+        prev = current
+    return current
+
 
 start = time.time()
-recursive4 = factorial_recursive(4)
+fib3 = fibonacci(3)
 end = time.time()
 print (end - start)
 
-# beware of the maximum recursion depth!
-for i in range(1, 1010):
-    start = time.time()
-    factorial(i)
-    end = time.time()
-    print("Iterative calculation of " + str(i) + " took " + str(end - start))
+# for i in range(1, 1010):
+#     start = time.time()
+#     factorial(i)
+#     end = time.time()
+#     print("Iterative calculation of " + str(i) + " took " + str(end - start))
 
-    start = time.time()
-    factorial_recursive(i)
-    end = time.time()
-    print("Recursive calculation of " + str(i) + " took " + str(end - start))
+#     start = time.time()
+#     factorial_recursive(i)
+#     end = time.time()
+#     print("Recursive calculation of " + str(i) + " took " + str(end - start))
 
-print(recursive4)
-
-fact4 = factorial(5)
-print(fact4)
-
-fact1 = factorial(1)
-print(fact1)
-
-fact0 = factorial(0)
-print(fact0)
