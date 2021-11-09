664fae1fa7cb4f3b53d3c53b4a3d8de2d4d33f9e
zctaskin <zctaskin@gmail.com>
2021-11-09 14:52:38 +0300

    polymorphism

--- hello.py
+++ hello.py
@@ -8,6 +8,9 @@ class Rectangle:
             for j in range(self._Width):
                 print("*", end="")
             print("")
+    
+    def overridden_function(self):
+        print("This is Rectangle's overridden_function")
 
 class Square:
     def __init__(self, length) -> None:
@@ -26,6 +29,11 @@ class NewSquare(Rectangle):
     def my_additional_function(self):
         print("This function belongs to NewSquare")
 
+    # function override mechanism
+    def overridden_function(self):
+        super().overridden_function()
+        print("This is NewSquare's overridden_function")
+
 print("Rectangle:")
 rect = Rectangle(5, 8)
 rect.print()
@@ -38,6 +46,34 @@ print("NewSquare:")
 nsqr = NewSquare(6)
 nsqr.print()
 nsqr.my_additional_function()
+nsqr.overridden_function()
 
 # my_additional_function does not exist at Rectangle
 # rect.my_additional_function()
+rect.overridden_function()
+
+class Animal:
+    def talk(self):
+        pass
+
+class Dog(Animal):
+    def talk(self):
+        print("Hav")
+
+class Cat(Animal):
+    def talk(self):
+        print("Miyav")
+
+class Bird(Animal):
+    def talk(self):
+        print("Cik")
+
+my_dog = Dog()
+my_cat = Cat()
+my_bird = Bird()
+
+# Polymorphism
+my_pets = [my_dog, my_cat, my_bird]
+for pet in my_pets:
+    pet.talk()
+
