2cc2b1f6c0a955f824eae127587adc3b749127ea
zctaskin <zctaskin@gmail.com>
2021-11-25 09:27:39 +0300

    initial definitions

--- hello.py
+++ hello.py
@@ -1,78 +1,36 @@
-import ctypes
-class Array:
+# insert
+# find
+# size
+# remove
+# print
+
+class ListNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+class LinkedList:
     def __init__(self):
-        self._array = None
-        self._length = 0
+        self.root = None
     
-    # O(n)
     def insert(self, value):
-        # allocate new array -> O(1)
-        new_array = ((self._length + 1) * ctypes.py_object)()
+        pass
 
-        # copy existing items -> O(n)
-        for i in range(self._length):
-            new_array[i] = self._array[i]
 
-        # insert the new item at the end -> O(1)
-        new_array[self._length] = value
+m = LinkedList()
 
-        # bookkeeping -> O(1)
-        self._length += 1
-        self._array = new_array
+# m.insert(4)
+# m.print()
 
-    # similar to in operator
-    # O(n)
-    def find(self, value):
-        for i in range(self._length):
-            if self._array[i] == value:
-                return True
-        return False
+# m.insert(6)
+# m.print()
 
-    # O(1)
-    def size(self):
-        return self._length
-    
-    # O(n)
-    def remove_index(self, index):
-        # allocate new array -> O(1)
-        new_array = ((self._length - 1) * ctypes.py_object)()
+# find1 = m.find(6)
+# print(find1)
 
-        # copy existing items before index -> O(n)
-        for i in range(index):
-            new_array[i] = self._array[i]
+# find2 = m.find(-6)
+# print(find2)
 
-        # copy existing items after index -> O(n)
-        for i in range(index, self._length - 1):
-            new_array[i] = self._array[i+1]
-
-        # bookkeeping -> O(1)
-        self._length -= 1
-        self._array = new_array
-
-    # O(1)
-    def get_index(self, index):
-        return self._array[index]
-
-    def print(self):
-        print("[", end="")
-        for i in range(self._length):
-            print(self._array[i], end="")
-        print("]")
-
-m = Array()
-m.insert(4)
-m.print()
-
-m.insert(6)
-m.print()
-
-find1 = m.find(6)
-print(find1)
-
-find2 = m.find(-6)
-print(find2)
-
-m.remove_index(0)
-m.print()
+# m.remove_index(0)
+# m.print()
 
 i = 4
