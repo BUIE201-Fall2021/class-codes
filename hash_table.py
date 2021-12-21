178fdd3d9acb6acebd92e924c9ff99b70a0018d0
zctaskin <zctaskin@gmail.com>
2021-12-21 13:53:55 +0300

    insert

---
+++ hash_table.py
@@ -0,0 +1,30 @@
+print(hash("test"))
+print(hash("test"))
+
+print(hash("test "))
+
+print(hash(-100))
+
+print(hash(0.423424))
+print(hash(0.423425))
+
+# collision
+print(hash(0.1))
+print(hash(230584300921369408))
+
+class HashTable:
+    def __init__(self) -> None:
+        self._bucket_size = 100
+        self._array = [None] * self._bucket_size
+    
+    def insert(self, value):
+        hash_value = hash(value)
+        index = hash_value % self._bucket_size
+        if self._array[index] == None:
+            self._array[index] = []
+        self._array[index].append(value)
+
+h = HashTable()
+h.insert("IE 201")
+h.insert("IE 203")
+h.insert("IE 515")
