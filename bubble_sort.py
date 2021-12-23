00c61394599ba1942ad98c99789d41d693f2f15e
zctaskin <zctaskin@gmail.com>
2021-12-23 10:22:10 +0300

    bubble sort implementation

---
+++ bubble_sort.py
@@ -0,0 +1,14 @@
+def bubble_sort(l):
+    print(l)
+    while True:
+        already_sorted = True
+        for i in range(len(l) - 1):
+            if l[i] > l[i+1]:
+                already_sorted = False
+                l[i], l[i+1] = l[i + 1], l[i]
+                print(l)
+        if already_sorted:
+            return
+
+l = [3, 10, 5, 2, -1, 15]
+bubble_sort(l)
