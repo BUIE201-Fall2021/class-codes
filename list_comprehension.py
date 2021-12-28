33e08bdb4805d3c3ae03f04eb2338c9e25d87de3
zctaskin <zctaskin@gmail.com>
2021-12-28 13:42:14 +0300

    if in list comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -12,3 +12,24 @@ print(list_uppercase)
 list_comp_uppercase = [x.upper() for x in list_lowercase]
 
 print(list_comp_uppercase)
+
+
+# create a list that consists of squares of the first 10 integers
+
+list_squares_explicit = []
+for i in range(10):
+    list_squares_explicit.append(i * i)
+
+list_squares = [i * i for i in range(10)]
+print(list_squares)
+
+# create a list that consists of squares of the first 5 odd integers
+list_squares_odd_explicit = []
+for i in range(10):
+    if i % 2:
+        list_squares_odd_explicit.append(i * i)
+print (list_squares_odd_explicit)
+
+# equivalent to
+list_squares_odd = [i * i for i in range(10) if i % 2]
+print (list_squares_odd)
