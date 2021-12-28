5ab656e15df2133337ed40fd1bcc5d9ee1d82e7f
zctaskin <zctaskin@gmail.com>
2021-12-28 13:30:06 +0300

    list comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,8 +1,14 @@
 list_lowercase = ["tomato", "potato", "cabbage", "lettuce"]
 print(list_lowercase)
 
+# explicit list creation loop
 list_uppercase = []
 for x in list_lowercase:
     list_uppercase.append(x.upper())
 
 print(list_uppercase)
+
+# equivalent list comprehension
+list_comp_uppercase = [x.upper() for x in list_lowercase]
+
+print(list_comp_uppercase)
