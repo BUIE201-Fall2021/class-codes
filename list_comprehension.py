9727192dfde8e7c514dec60ddcbaea77221709f6
zctaskin <zctaskin@gmail.com>
2021-12-28 13:22:02 +0300

    initial version

---
+++ list_comprehension.py
@@ -0,0 +1,8 @@
+list_lowercase = ["tomato", "potato", "cabbage", "lettuce"]
+print(list_lowercase)
+
+list_uppercase = []
+for x in list_lowercase:
+    list_uppercase.append(x.upper())
+
+print(list_uppercase)
